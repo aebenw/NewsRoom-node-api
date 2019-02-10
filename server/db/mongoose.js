3675f3cf5ce00d02be4965755486e1da59b1c633
github username <github email address>
2019-02-10 13:00:06 -0500

    fetches favicon

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -4,6 +4,7 @@ mongoose.promise = global.Promise;
 mongoose.connect(process.env.MONGODB_URI);
 const db = mongoose.connection;
 // db.dropDatabase()
+// db.dropCollection('sources')
 db.on('error', err => console.log(err));
 db.on('open', () => console.log(`success on ${process.port}`));
 
---
+++ server/models/classes/Favicon.js
@@ -0,0 +1,14 @@
+import axios from 'axios';
+
+const baseURL = "https://besticon-demo.herokuapp.com/"
+
+async function getImageURL (url){
+  let image = await axios.get(`${baseURL}icon?url=${url}&size=50..150..300`)
+
+  return image.request.res.responseUrl
+  // return image
+}
+
+export {
+  getImageURL
+}
