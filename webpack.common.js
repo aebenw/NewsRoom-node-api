1b777aca73844902600c08ca91bc637a5d797a59
github username <github email address>
2019-02-25 17:06:45 -0500

    realizing dev server is pointless

---
+++ webpack.common.js
@@ -0,0 +1,31 @@
+const path = require('path');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+var nodeExternals = require('webpack-node-externals');
+
+module.exports = {
+  entry: {main: ['@babel/polyfill', './server/server.js']},
+  plugins: [
+    new CleanWebpackPlugin(['build'])
+  ],
+  target: 'node',
+  externals: [nodeExternals()],
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ['@babel/preset-env'],
+            plugins: ['@babel/plugin-proposal-object-rest-spread']
+          }
+        }
+      }
+    ]
+  },
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    filename: '[name].js'
+  }
+};
